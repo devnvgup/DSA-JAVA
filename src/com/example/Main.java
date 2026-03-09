715bf0375a92a871a7a612fe2350270e27f0fbd1
devnvgup <truongnguyen16999@gmail.com>
2026-03-09 22:16:58 +0700

    Update

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,7 +2,10 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-       boolean result = ValidAnagram.isAnagram("racecar", "carrace");
+    //    boolean result = ValidAnagram.isAnagram("racecar", "carrace");
+       int[] nums = new int[]{1,2,2,3,3,3};
+       int k = 2;
+       int[] result = TopKFrequentElements.topKFrequent(nums, k);
        System.out.println(result); // Output: true
     }
 }
---
+++ src/com/example/TopKFrequentElements.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class TopKFrequentElements {
+
+    public static int[] topKFrequent(int[] nums, int k) {
+        // Input: nums = [1,2,2,3,3,3], k = 2
+        // Output: [2,3]
+        Map<Integer, Integer> map = new HashMap();
+        for (int el : nums) {
+            if (!map.containsKey(el)) {
+                map.put(el, 1);
+            } else {
+                int value = map.get(el);
+                map.put(el, ++value);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
+        Map<Integer, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<Integer, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        List<Integer> keys = new ArrayList<>(sortedMap.keySet());
+        List<Integer> sublist = keys.subList(keys.size() - k, keys.size());
+        int[] arr = new int[sublist.size()];
+        for (int i = 0; i < sublist.size(); i++) {
+            arr[i] = sublist.get(i);
+        }
+        return arr;
+    }
+}
